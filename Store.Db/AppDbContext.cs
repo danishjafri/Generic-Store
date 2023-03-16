021f20a1d4dc3ca24b27a9ebd404545693a8996c
Danish Jafri <danishjafri@outlook.com>
2023-03-16 13:44:37 +0000

    Added Context class for database. Added Order, Product and Order Item linking table.

---
+++ Store.Db/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+using Store.Models;
+
+namespace Store.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure the entity relationships and constraints
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Store.Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace Store.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; } = new ();
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+
+}
