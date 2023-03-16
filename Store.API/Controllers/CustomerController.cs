e1d42a6a7b3596da3d3fad4c05a5caee1d1a9d2c
Danish Jafri <danishjafri@outlook.com>
2023-03-16 13:58:22 +0000

    Added test for repository.

---
+++ Store.API/Controllers/CustomerController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Store.Models;
+using Store.Repositories.Promises;
+
+namespace Store.API.Controllers
+{
+    [ApiController]
+    [Route("api/customers")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IRepository<Customer> _customerRepository;
+
+        public CustomerController(IRepository<Customer> customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetAllCustomers()
+        {
+            var customers = await _customerRepository.GetAllAsync();
+            return Ok(customers);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomerById(int id)
+        {
+            var customer = await _customerRepository.GetByIdAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCustomer(Customer customer)
+        {
+            await _customerRepository.AddAsync(customer);
+            return CreatedAtAction(nameof(GetCustomerById), new { id = customer.Id }, customer);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCustomer(int id, Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return BadRequest();
+            }
+
+            await _customerRepository.UpdateAsync(customer);
+            return NoContent();
+        }
+
+    }
+}
---
+++ Store.Repositories.Tests/CustomerRepositoryTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using Store.Db;
+using Store.Models;
+using Store.Repositories.Implementations;
+using Store.Repositories.Promises;
+
+[TestFixture]
+public class CustomerRepositoryTests
+{
+    private Mock<AppDbContext> _dbContextMock;
+    private IRepository<Customer> _customerRepository;
+
+    [SetUp]
+    public void Setup()
+    {
+        _dbContextMock = new Mock<AppDbContext>(new DbContextOptions<AppDbContext>());
+        _customerRepository = new CustomerRepository(_dbContextMock.Object);
+    }
+
+    [Test]
+    public async Task GetAllAsync_ReturnsAllCustomers()
+    {
+        // Arrange
+        var customers = new List<Customer>
+         {
+             new Customer { Id = 1, FirstName = "John", LastName = "Doe" },
+             new Customer { Id = 2, FirstName = "Jane", LastName = "Doe" },
+             new Customer { Id = 3, FirstName = "Bob", LastName = "Smith" }
+         };
+        _dbContextMock.Setup(c => c.Customers.ToListAsync(default(CancellationToken))).ReturnsAsync(customers);
+
+        // Act
+        var result = await _customerRepository.GetAllAsync();
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Count(), Is.EqualTo(customers.Count));
+        Assert.That(result, Is.EquivalentTo(customers));
+    }
+
+    // Add more tests for GetByIdAsync, AddAsync, UpdateAsync, and DeleteAsync methods
+}
