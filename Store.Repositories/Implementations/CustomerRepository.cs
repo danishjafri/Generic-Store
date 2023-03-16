bea750a3fe9d1e5453fd3e7ce1bbf6773590894d
Danish Jafri <danishjafri@outlook.com>
2023-03-16 13:46:32 +0000

    Added customer repository.

---
+++ Store.Repositories/Implementations/CustomerRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Store.Db;
+using Store.Models;
+using Store.Repositories.Promises;
+
+namespace Store.Repositories.Implementations
+{
+    public class CustomerRepository : IRepository<Customer>
+    {
+        private readonly AppDbContext _dbContext;
+
+        public CustomerRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Customer>> GetAllAsync()
+        {
+            return await _dbContext.Customers.ToListAsync();
+        }
+
+        public async Task<Customer> GetByIdAsync(int id)
+        {
+            return await _dbContext.Customers.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task AddAsync(Customer customer)
+        {
+            await _dbContext.Customers.AddAsync(customer);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Customer customer)
+        {
+            _dbContext.Entry(customer).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Customer customer)
+        {
+            _dbContext.Customers.Remove(customer);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+
+}
